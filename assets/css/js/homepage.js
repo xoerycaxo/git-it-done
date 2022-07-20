39a4d5ce101c4ab1f861710eed7cbf7134604dee
boy-mom <100622402+xoerycaxo@users.noreply.github.com>
2022-07-20 11:24:42 -0400

    server-side api

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -1,5 +1,15 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos")
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
   };
   
-  getUserRepos();
+  getUserRepos("xoerycaxo");
+  
